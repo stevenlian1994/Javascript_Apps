74d71c687eff12e98752f909bd842534408e7e7b
Steven Lian <stevenlian1994@gmail.com>
2017-10-19 07:33:37 +0000

    added login and signup, now need to practice and understand the logic

--- dog_bulletin/v2/index.js
+++ dog_bulletin/v2/index.js
@@ -1,9 +1,12 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose")
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    User = require("./models/user")
  
-    
+
 
     
 //APP CONFIG
@@ -13,10 +16,83 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty cutest",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
+
  app.get("/", function(req, res){
     res.render("landing"); 
  });
     
+app.get("/welcome_page", isLoggedIn, function(req, res){
+    res.render("welcome_page");
+});
+    
+    
+
+// ============
+// AUTH ROUTES
+// ============
+app.get("/register", function(req, res){
+    res.render("register");
+})
+
+//handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/welcome_page");
+        });
+    });
+});
+
+//show login form
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+//handling login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/welcome_page",
+        failureRedirect: "/login"
+    }), function(req, res){
+    
+});
+
+//logic route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
---
+++ dog_bulletin/v2/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String
+});
+
+//what does this do??
+userSchema.plugin(passportLocalMongoose);
+
+//and this?
+module.exports = mongoose.model("User", userSchema);
